477383712749e7484e9b5fc6736d22ed33ea8786
Philipp Hollenstein <philipp.hollenstein95@gmail.com>
2017-10-10 16:04:04 +0200

    praktikum 4 finished

--- src/ch/zhaw/ads/praktikum4/HilbertServer.java
+++ src/ch/zhaw/ads/praktikum4/HilbertServer.java
@@ -3,9 +3,48 @@ package ch.zhaw.ads.praktikum4;
 import ch.zhaw.ads.CommandExecutor;
 
 public class HilbertServer implements CommandExecutor {
+    private Turtle turtle;
 
     @Override
     public String execute(String command) throws Exception {
-        return null;
+        int depth = Integer.parseInt(command);
+        double dist = 0.8 / (Math.pow(2, depth + 1) - 1);
+        turtle = new Turtle(0.1, 0.1);
+        hilbert(depth, dist, -90);
+
+        return turtle.getTrace();
     }
+
+    private void hilbert(int depth,
+                         double dist, double angle) {
+        if(depth < 0){
+            return;
+        }
+
+        turtle.turn(-angle);
+
+        // draw recursive
+        hilbert(depth - 1, dist, -angle);
+
+        turtle.move(dist);
+        turtle.turn(angle);
+
+        // draw recursive
+        hilbert(depth - 1, dist, angle);
+
+        turtle.move(dist);
+
+        // draw recursive
+        hilbert(depth - 1, dist, angle);
+
+        turtle.turn(angle);
+        turtle.move(dist);
+
+        // draw recursive
+        hilbert(depth - 1, dist, -angle);
+
+        turtle.turn(-angle);
+
+    }
+
 }
--- src/ch/zhaw/ads/praktikum4/SnowflakeServer.java
+++ src/ch/zhaw/ads/praktikum4/SnowflakeServer.java
@@ -3,7 +3,7 @@ package ch.zhaw.ads.praktikum4;
 import ch.zhaw.ads.CommandExecutor;
 
 public class SnowflakeServer implements CommandExecutor {
-    Turtle turtle;
+    private Turtle turtle;
 
     @Override
     public String execute(String command) throws Exception {
