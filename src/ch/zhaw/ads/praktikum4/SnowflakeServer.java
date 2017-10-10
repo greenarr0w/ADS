3b17a8f329a4db09c8e4b703bb3249e151e9abfd
Philipp Hollenstein <philipp.hollenstein95@gmail.com>
2017-10-10 15:41:08 +0200

    snowflake

--- src/ch/zhaw/ads/praktikum4/SnowflakeServer.java
+++ src/ch/zhaw/ads/praktikum4/SnowflakeServer.java
@@ -3,15 +3,21 @@ package ch.zhaw.ads.praktikum4;
 import ch.zhaw.ads.CommandExecutor;
 
 public class SnowflakeServer implements CommandExecutor {
-    final Turtle turtle = new Turtle(0, 0.5);
+    Turtle turtle;
 
     @Override
     public String execute(String command) throws Exception {
+        turtle = new Turtle(0.25, 0.75);
+
         final String[] split = command.split(" ");
         final int step = Integer.valueOf(split[0]);
         final double dist = Double.valueOf(split[1]);
 
-        snowflake(step, dist);
+
+        for (int i = 0; i < 3; i++) {
+            snowflake(step, dist);
+            turtle.turn(-120);
+        }
         return turtle.getTrace();
     }
 
