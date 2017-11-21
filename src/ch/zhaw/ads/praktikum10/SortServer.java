aeb122e2dc89b99753bde7e4c152909f731498ab
Philipp Hollenstein <philipp.hollenstein95@gmail.com>
2017-11-21 16:01:45 +0100

    praktikum 10 v2

--- src/ch/zhaw/ads/praktikum10/SortServer.java
+++ src/ch/zhaw/ads/praktikum10/SortServer.java
@@ -2,14 +2,30 @@ package ch.zhaw.ads.praktikum10;
 
 import ch.zhaw.ads.CommandExecutor;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.function.Consumer;
 
 public class SortServer implements CommandExecutor {
 
     @Override
     public String execute(String command) throws Exception {
         //int length = 1000000;
-        int length = Integer.valueOf(command);
+        String separator = " ";
+        String[] split = command.split(separator);
+        String action = split[0];
+        Consumer<int[]> consumer;
+        if(action.toLowerCase().equals("insertion")) {
+            consumer = this::insertionSort;
+        } else if(action.toLowerCase().equals("bubble")) {
+            consumer = this::bubbleSortComplete;
+        } else if(action.toLowerCase().equals("selection")) {
+            consumer = this::selectionSort;
+        } else {
+            throw new IllegalArgumentException("Wrong command");
+        }
+
+        int length = Integer.valueOf(split[1]);
         int[] intArray = new int[length + 1];
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = new Random().nextInt(length);
@@ -20,8 +36,7 @@ public class SortServer implements CommandExecutor {
         do {
             int[] copy = new int[intArray.length];
             System.arraycopy(intArray, 0, copy, 0, intArray.length);
-            bubbleSortComplete(copy);
-
+            consumer.accept(copy);
             count++;
             end = System.currentTimeMillis();
         } while (end - start < 1000);
@@ -32,25 +47,50 @@ public class SortServer implements CommandExecutor {
         boolean sorted = checkSorted(intArray);
 
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("is sorted: ").append(sorted).append("\n")
+        stringBuilder.append(action).append(" is sorted: ").append(sorted).append("\n")
                 .append(print).append("\n");
+        Arrays.stream(intArray).forEach(value -> stringBuilder.append(value).append("\n"));
         return stringBuilder.toString();
     }
 
-    private static void bubbleSortComplete(int[] intArray) {
-        boolean hasSwaped = false;
+    private void bubbleSortComplete(int[] intArray) {
+        boolean hasSwapped = false;
         for (int i = 0; i < intArray.length - 1; i++) {
             if (intArray[i] > intArray[i + 1]) {
                 swap(intArray, i, i + 1);
-                hasSwaped = true;
+                hasSwapped = true;
             }
         }
-        if (hasSwaped) {
+        if (hasSwapped) {
             bubbleSortComplete(intArray);
         }
     }
 
+    private void selectionSort(int[] intArray) {
+        for (int k = 0; k < intArray.length; k++) {
+            int min = k;
+            for (int i = k + 1; i < intArray.length; i++) {
+                if (intArray[i] < intArray[min]) {
+                    min = i;
+                }
+            }
+            if (min != k) {
+                swap(intArray, min, k);
+            }
+        }
+    }
 
+    private void insertionSort(int[] intArray) {
+        for (int k = 1; k < intArray.length; k++)
+            if (intArray[k] < intArray[k - 1]) {
+                int x = intArray[k];
+                int i;
+                for (i = k; ((i > 0) && (intArray[i - 1] > x)); i--) {
+                    intArray[i] = intArray[i - 1];
+                }
+                intArray[i] = x;
+            }
+    }
 
     private static void swap(int[] intArray, int index1, int index2) {
         int tmp = intArray[index1];
